a0fbfa9b59b92ea6a4f70e9f795f74d4625e9712
ki <ki@ki.ki>
2018-09-15 20:02:40 +0800

    ki

--- mvc/controller/Wechat/Index.php
+++ mvc/controller/Wechat/Index.php
@@ -1,13 +1,8 @@
 <?php
 
-class App_Controller_Admin_Index{
+class App_Controller_Wechat_Index{
     public function indexAction(){
         echo 'admin,index';
-        $ctr =new Controller();
-        $db = new Mysql('member');
-        $size =  $db->getCount();
-        $ctr->assign('size',$size);
-        $ctr->DisplaySmart("Index/index.html");
     }
     public function testAction(){
      
