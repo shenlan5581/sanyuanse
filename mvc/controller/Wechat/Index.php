075a843eeaf64df0e6ad5ade1adefed921746bb3
ki <ki@ki.ki>
2018-09-15 23:19:55 +0800

    ki

--- mvc/controller/Wechat/Index.php
+++ mvc/controller/Wechat/Index.php
@@ -3,29 +3,68 @@
 class App_Controller_Wechat_Index{
     public function indexAction(){
         
-   $signature  =   $_GET["signature"];
-   $timestamp  =   $_GET["timestamp"];
-   $nonce      =   $_GET["nonce"];
-   
-    $token =  "sanyuanse";
-    $tmpArr = array($timestamp, $nonce,$token);
-    sort($tmpArr, SORT_STRING);
-    $tmpStr = implode( $tmpArr);
-    $tmpStr = sha1( $tmpStr );
-    
-    if( $signature = $tmpStr ){
-        echo $_GET['echostr'];
-    return true;
-    }else{
-    return false;
-    }
+
+
+
+
+
 
 
 
 
 
     }
+
     public function testAction(){
+
+        
+
+
+
+
+
+
+
+        
      
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //微信接入验证
+    private function WechatCheck($token){
+        $signature  =   $_GET["signature"];
+        $timestamp  =   $_GET["timestamp"];
+        $nonce      =   $_GET["nonce"];
+        $tmpArr = array($timestamp, $nonce,$token);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr);
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $signature = $tmpStr ){
+            echo $_GET['echostr'];
+        return true;
+        }else{
+        return false;
+        }
+    }
+
+
+
+
 }
