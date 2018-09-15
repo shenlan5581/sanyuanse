8da91c25deb12d06545819db28e8551a852b8a3b
ki <ki@ki.ki>
2018-09-16 00:06:51 +0800

    ki

--- mvc/controller/Wechat/Index.php
+++ mvc/controller/Wechat/Index.php
@@ -1,20 +1,24 @@
 <?php
+//weixin公众号
 
 class App_Controller_Wechat_Index{
-    public function indexAction(){
+   public $ctr;
+   public function __construct(){
+     $this->$ctr=new Controller;
+   }
+   public function indexAction(){
+
+
+
+
+
+
+
+
+
         
-
-
-
-
-
-
-
-
-
-
+     $this->ctr->DisplaySmart('/Wechat/wx.html');
     }
-
     public function testAction(){
 
         
